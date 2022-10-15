6a81684affb96d927495891e774e48028b0a3dc1
anmolsinghsahi <anmol.sahi@mutualmobile.com>
2022-10-15 12:35:24 +0530

    added mock nutrients data

--- wearable/src/main/java/com/example/myfitnesspalclone/presentation/screens/nutrients/NutrientsScreen.kt
+++ wearable/src/main/java/com/example/myfitnesspalclone/presentation/screens/nutrients/NutrientsScreen.kt
@@ -1,22 +1,27 @@
 package com.example.myfitnesspalclone.presentation.screens.nutrients
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.PositionIndicator
 import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.ScalingLazyColumn
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.rememberScalingLazyListState
-import com.example.myfitnesspalclone.presentation.navigation.Screens
+import com.example.myfitnesspalclone.presentation.data.mockNutrientsList
 import com.example.myfitnesspalclone.presentation.screens.summarylist.TopTitle
+import com.example.myfitnesspalclone.presentation.theme.Red400
 import com.example.myfitnesspalclone.presentation.utils.VerticalSpacer
-import com.google.android.material.progressindicator.LinearProgressIndicator
 
 @Composable
 fun NutrientsScreen() {
@@ -37,15 +42,26 @@ fun NutrientsScreen() {
                 }
             }
 
-            items(15) {
-                NutrientRow()
+            items(mockNutrientsList.size) {
+                val currentNutrient = mockNutrientsList[it]
+                NutrientRow(
+                    name = currentNutrient.name,
+                    value = currentNutrient.value,
+                    target = currentNutrient.target,
+                    color = currentNutrient.color
+                )
             }
         }
     }
 }
 
 @Composable
-fun NutrientRow() {
+fun NutrientRow(
+    name: String,
+    value: Float,
+    target: Float,
+    color: Color = Color(0xFF232323)
+) {
     Column(modifier = Modifier.fillMaxSize()) {
         VerticalSpacer(height = 4.dp)
 
@@ -55,16 +71,17 @@ fun NutrientRow() {
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(text = "Protein")
-            Text(text = "0/419")
+            Text(text = name)
+            Text(text = "${value.toInt()}/${target.toInt()}")
         }
 
-        Box(
+        LinearProgressIndicator(
             modifier = Modifier
                 .padding(16.dp)
-                .height(2.dp)
-                .fillMaxWidth()
-                .background(color = Color.White)
+                .height(2.dp),
+            progress = value/target,
+            backgroundColor = color,
+            color = Red400
         )
 
     }
